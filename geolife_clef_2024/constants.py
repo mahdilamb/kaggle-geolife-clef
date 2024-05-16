6ddfde7f00fc48985eec15cd53454bf179ab7280
Mahdi Lamb <mahdilamb@gmail.com>
2024-05-16 11:57:42 +0100

    add env vars

--- geolife_clef_2024/constants.py
+++ geolife_clef_2024/constants.py
@@ -4,8 +4,12 @@ import os
 
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))  # The root of the package
 ROOT_DIR = os.path.dirname(PACKAGE_DIR)  # The parent to the package
-DATA_DIR = os.path.join(ROOT_DIR, "data")  # The location of the data
-CHECKPOINT_DIR = os.path.join(ROOT_DIR, "checkpoints")
+DATA_DIR = os.getenv(
+    "GEOLIFE_CLEF_DATA", os.path.join(ROOT_DIR, "data")
+)  # The location of the data
+CHECKPOINT_DIR = os.getenv(
+    "GEOLIFE_CLEF_CHECKPOINTS", os.path.join(ROOT_DIR, "checkpoints")
+)
 
 WANDB_PROJECT = "geolife-clef-2024"
 
